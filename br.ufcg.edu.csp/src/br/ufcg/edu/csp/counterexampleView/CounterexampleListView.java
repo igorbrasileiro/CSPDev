d13de26e52826e3a900551ed247564bf4e4d90b4
igorbrasileiro <brasileiro456@gmail.com>
2018-04-24 17:54:11 -0300

    add hook do web browser com contra exemplo - svg ainda não plotavel

--- br.ufcg.edu.csp/src/br/ufcg/edu/csp/counterexampleView/CounterexampleListView.java
+++ br.ufcg.edu.csp/src/br/ufcg/edu/csp/counterexampleView/CounterexampleListView.java
@@ -1,12 +1,22 @@
 package br.ufcg.edu.csp.counterexampleView;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.ListViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.browser.IWebBrowser;
+import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
 import org.eclipse.ui.part.ViewPart;
+
+import br.ufcg.edu.csp.CSPDocumentProvider;
+import br.ufcg.edu.csp.fdrAnalyser.CSPGraphServices;
 
 
 public class CounterexampleListView extends ViewPart {
@@ -14,6 +24,7 @@ public class CounterexampleListView extends ViewPart {
 	private ListViewer viewer;
 	private CheckerNodeListSingleton cnls;
 	private Action doubleClickAction;
+	private String projectFilePath;
 	
 	@Override
 	public void createPartControl(Composite parent) {
@@ -46,6 +57,7 @@ public class CounterexampleListView extends ViewPart {
 				Object obj = selection.getFirstElement();
 				// chamar o contra exemplo
 				System.out.println("Double-click detected on "+obj.toString());
+				openCounterexampleBrowser(((CheckerNodeDecorator) obj).getCounterexamples());
 			}
 		};
 	}
@@ -54,4 +66,40 @@ public class CounterexampleListView extends ViewPart {
 		viewer.addDoubleClickListener(o -> doubleClickAction.run());
 	}
 	
+	private void openCounterexampleBrowser(String[] nodes) {
+		try{
+			IWorkbenchBrowserSupport support = PlatformUI.getWorkbench().getBrowserSupport();
+			// trocar instancia caso queira um único browser
+			IWebBrowser browser = support.createBrowser(IWorkbenchBrowserSupport.AS_EDITOR, null, "CSP Editor", "Checker counterexample");
+			
+			createCounterExamplesFiles();
+			
+			CSPGraphServices cgs = new CSPGraphServices();
+			cgs.plotGraph(nodes);
+			
+			File file = new File(projectFilePath+"/Outputs/counterExamplePage.html");
+			
+			URL urlFile = file.toURI().toURL();
+			
+			System.out.println(file);
+			System.out.println(urlFile);
+			
+			browser.openURL(urlFile);
+			
+		} catch (Exception ceb) {
+			ceb.printStackTrace();
+			System.out.println("erro counterexample list abrir pagina web");
+		}
+		
+	}
+	
+	private void createCounterExamplesFiles() throws IOException {
+		projectFilePath = CSPDocumentProvider.getEditorFile().getParentFile().getAbsolutePath();
+
+		File f = new File(projectFilePath+"/Outputs");
+		f.mkdir();
+		
+		//System.out.println(f.getAbsolutePath());
+		//FileWriter fw = new FileWriter(f.getAbsoluteFile()+"/counterExampleGraph.json")
+	}
 }
