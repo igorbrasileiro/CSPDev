d1849c14d4d783190590da0af4bb8e4e354a0231
igorbrasileiro <brasileiro456@gmail.com>
2018-05-09 22:24:59 -0300

    update parser and removed Csp.g4 grammar to new repository

--- br.ufcg.edu.csp/src/br/ufcg/edu/csp/parser/CspListener.java
+++ br.ufcg.edu.csp/src/br/ufcg/edu/csp/parser/CspListener.java
@@ -1,6 +1,6 @@
-// Generated from Csp.g4 by ANTLR 4.4
 package br.ufcg.edu.csp.parser;
-import org.antlr.v4.runtime.misc.NotNull;
+
+// Generated from Csp.g4 by ANTLR 4.7
 import org.antlr.v4.runtime.tree.ParseTreeListener;
 
 /**
@@ -9,183 +9,233 @@ import org.antlr.v4.runtime.tree.ParseTreeListener;
  */
 public interface CspListener extends ParseTreeListener {
 	/**
-	 * Enter a parse tree produced by {@link CspParser#channelDecl}.
-	 * @param ctx the parse tree
-	 */
-	void enterChannelDecl(@NotNull CspParser.ChannelDeclContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link CspParser#channelDecl}.
-	 * @param ctx the parse tree
-	 */
-	void exitChannelDecl(@NotNull CspParser.ChannelDeclContext ctx);
-	/**
-	 * Enter a parse tree produced by {@link CspParser#proc}.
-	 * @param ctx the parse tree
-	 */
-	void enterProc(@NotNull CspParser.ProcContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link CspParser#proc}.
-	 * @param ctx the parse tree
-	 */
-	void exitProc(@NotNull CspParser.ProcContext ctx);
-	/**
-	 * Enter a parse tree produced by {@link CspParser#set}.
-	 * @param ctx the parse tree
-	 */
-	void enterSet(@NotNull CspParser.SetContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link CspParser#set}.
-	 * @param ctx the parse tree
-	 */
-	void exitSet(@NotNull CspParser.SetContext ctx);
-	/**
-	 * Enter a parse tree produced by {@link CspParser#definitionLeft}.
-	 * @param ctx the parse tree
-	 */
-	void enterDefinitionLeft(@NotNull CspParser.DefinitionLeftContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link CspParser#definitionLeft}.
-	 * @param ctx the parse tree
-	 */
-	void exitDefinitionLeft(@NotNull CspParser.DefinitionLeftContext ctx);
-	/**
-	 * Enter a parse tree produced by {@link CspParser#boolExp}.
-	 * @param ctx the parse tree
-	 */
-	void enterBoolExp(@NotNull CspParser.BoolExpContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link CspParser#boolExp}.
-	 * @param ctx the parse tree
-	 */
-	void exitBoolExp(@NotNull CspParser.BoolExpContext ctx);
-	/**
-	 * Enter a parse tree produced by {@link CspParser#simple}.
-	 * @param ctx the parse tree
-	 */
-	void enterSimple(@NotNull CspParser.SimpleContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link CspParser#simple}.
-	 * @param ctx the parse tree
-	 */
-	void exitSimple(@NotNull CspParser.SimpleContext ctx);
-	/**
-	 * Enter a parse tree produced by {@link CspParser#simpleDefinition}.
-	 * @param ctx the parse tree
-	 */
-	void enterSimpleDefinition(@NotNull CspParser.SimpleDefinitionContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link CspParser#simpleDefinition}.
-	 * @param ctx the parse tree
-	 */
-	void exitSimpleDefinition(@NotNull CspParser.SimpleDefinitionContext ctx);
-	/**
-	 * Enter a parse tree produced by {@link CspParser#boolOrAmb}.
-	 * @param ctx the parse tree
-	 */
-	void enterBoolOrAmb(@NotNull CspParser.BoolOrAmbContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link CspParser#boolOrAmb}.
-	 * @param ctx the parse tree
-	 */
-	void exitBoolOrAmb(@NotNull CspParser.BoolOrAmbContext ctx);
-	/**
-	 * Enter a parse tree produced by {@link CspParser#channelColonType}.
-	 * @param ctx the parse tree
-	 */
-	void enterChannelColonType(@NotNull CspParser.ChannelColonTypeContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link CspParser#channelColonType}.
-	 * @param ctx the parse tree
-	 */
-	void exitChannelColonType(@NotNull CspParser.ChannelColonTypeContext ctx);
-	/**
-	 * Enter a parse tree produced by {@link CspParser#type}.
-	 * @param ctx the parse tree
-	 */
-	void enterType(@NotNull CspParser.TypeContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link CspParser#type}.
-	 * @param ctx the parse tree
-	 */
-	void exitType(@NotNull CspParser.TypeContext ctx);
-	/**
-	 * Enter a parse tree produced by {@link CspParser#any}.
-	 * @param ctx the parse tree
-	 */
-	void enterAny(@NotNull CspParser.AnyContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link CspParser#any}.
-	 * @param ctx the parse tree
-	 */
-	void exitAny(@NotNull CspParser.AnyContext ctx);
-	/**
 	 * Enter a parse tree produced by {@link CspParser#spec}.
 	 * @param ctx the parse tree
 	 */
-	void enterSpec(@NotNull CspParser.SpecContext ctx);
+	void enterSpec(CspParser.SpecContext ctx);
 	/**
 	 * Exit a parse tree produced by {@link CspParser#spec}.
 	 * @param ctx the parse tree
 	 */
-	void exitSpec(@NotNull CspParser.SpecContext ctx);
-	/**
-	 * Enter a parse tree produced by {@link CspParser#number}.
-	 * @param ctx the parse tree
-	 */
-	void enterNumber(@NotNull CspParser.NumberContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link CspParser#number}.
-	 * @param ctx the parse tree
-	 */
-	void exitNumber(@NotNull CspParser.NumberContext ctx);
-	/**
-	 * Enter a parse tree produced by {@link CspParser#channelNames}.
-	 * @param ctx the parse tree
-	 */
-	void enterChannelNames(@NotNull CspParser.ChannelNamesContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link CspParser#channelNames}.
-	 * @param ctx the parse tree
-	 */
-	void exitChannelNames(@NotNull CspParser.ChannelNamesContext ctx);
-	/**
-	 * Enter a parse tree produced by {@link CspParser#defnCallLeft}.
-	 * @param ctx the parse tree
-	 */
-	void enterDefnCallLeft(@NotNull CspParser.DefnCallLeftContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link CspParser#defnCallLeft}.
-	 * @param ctx the parse tree
-	 */
-	void exitDefnCallLeft(@NotNull CspParser.DefnCallLeftContext ctx);
+	void exitSpec(CspParser.SpecContext ctx);
+
 	/**
 	 * Enter a parse tree produced by {@link CspParser#definition}.
 	 * @param ctx the parse tree
 	 */
-	void enterDefinition(@NotNull CspParser.DefinitionContext ctx);
+	void enterDefinition(CspParser.DefinitionContext ctx);
 	/**
 	 * Exit a parse tree produced by {@link CspParser#definition}.
 	 * @param ctx the parse tree
 	 */
-	void exitDefinition(@NotNull CspParser.DefinitionContext ctx);
+	void exitDefinition(CspParser.DefinitionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#channelDecl}.
+	 * @param ctx the parse tree
+	 */
+	void enterChannelDecl(CspParser.ChannelDeclContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#channelDecl}.
+	 * @param ctx the parse tree
+	 */
+	void exitChannelDecl(CspParser.ChannelDeclContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#channelNames}.
+	 * @param ctx the parse tree
+	 */
+	void enterChannelNames(CspParser.ChannelNamesContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#channelNames}.
+	 * @param ctx the parse tree
+	 */
+	void exitChannelNames(CspParser.ChannelNamesContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#channelColonType}.
+	 * @param ctx the parse tree
+	 */
+	void enterChannelColonType(CspParser.ChannelColonTypeContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#channelColonType}.
+	 * @param ctx the parse tree
+	 */
+	void exitChannelColonType(CspParser.ChannelColonTypeContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#simpleDefinition}.
+	 * @param ctx the parse tree
+	 */
+	void enterSimpleDefinition(CspParser.SimpleDefinitionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#simpleDefinition}.
+	 * @param ctx the parse tree
+	 */
+	void exitSimpleDefinition(CspParser.SimpleDefinitionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#assertDefinition}.
+	 * @param ctx the parse tree
+	 */
+	void enterAssertDefinition(CspParser.AssertDefinitionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#assertDefinition}.
+	 * @param ctx the parse tree
+	 */
+	void exitAssertDefinition(CspParser.AssertDefinitionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#definitionLeft}.
+	 * @param ctx the parse tree
+	 */
+	void enterDefinitionLeft(CspParser.DefinitionLeftContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#definitionLeft}.
+	 * @param ctx the parse tree
+	 */
+	void exitDefinitionLeft(CspParser.DefinitionLeftContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#defnCallLeft}.
+	 * @param ctx the parse tree
+	 */
+	void enterDefnCallLeft(CspParser.DefnCallLeftContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#defnCallLeft}.
+	 * @param ctx the parse tree
+	 */
+	void exitDefnCallLeft(CspParser.DefnCallLeftContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#any}.
+	 * @param ctx the parse tree
+	 */
+	void enterAny(CspParser.AnyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#any}.
+	 * @param ctx the parse tree
+	 */
+	void exitAny(CspParser.AnyContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#checkConditionBody}.
+	 * @param ctx the parse tree
+	 */
+	void enterCheckConditionBody(CspParser.CheckConditionBodyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#checkConditionBody}.
+	 * @param ctx the parse tree
+	 */
+	void exitCheckConditionBody(CspParser.CheckConditionBodyContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#modelCheckType}.
+	 * @param ctx the parse tree
+	 */
+	void enterModelCheckType(CspParser.ModelCheckTypeContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#modelCheckType}.
+	 * @param ctx the parse tree
+	 */
+	void exitModelCheckType(CspParser.ModelCheckTypeContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#type}.
+	 * @param ctx the parse tree
+	 */
+	void enterType(CspParser.TypeContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#type}.
+	 * @param ctx the parse tree
+	 */
+	void exitType(CspParser.TypeContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#simple}.
+	 * @param ctx the parse tree
+	 */
+	void enterSimple(CspParser.SimpleContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#simple}.
+	 * @param ctx the parse tree
+	 */
+	void exitSimple(CspParser.SimpleContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#set}.
+	 * @param ctx the parse tree
+	 */
+	void enterSet(CspParser.SetContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#set}.
+	 * @param ctx the parse tree
+	 */
+	void exitSet(CspParser.SetContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#proc}.
+	 * @param ctx the parse tree
+	 */
+	void enterProc(CspParser.ProcContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#proc}.
+	 * @param ctx the parse tree
+	 */
+	void exitProc(CspParser.ProcContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#boolExp}.
+	 * @param ctx the parse tree
+	 */
+	void enterBoolExp(CspParser.BoolExpContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#boolExp}.
+	 * @param ctx the parse tree
+	 */
+	void exitBoolExp(CspParser.BoolExpContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#boolOrAmb}.
+	 * @param ctx the parse tree
+	 */
+	void enterBoolOrAmb(CspParser.BoolOrAmbContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#boolOrAmb}.
+	 * @param ctx the parse tree
+	 */
+	void exitBoolOrAmb(CspParser.BoolOrAmbContext ctx);
+
 	/**
 	 * Enter a parse tree produced by {@link CspParser#expr}.
 	 * @param ctx the parse tree
 	 */
-	void enterExpr(@NotNull CspParser.ExprContext ctx);
+	void enterExpr(CspParser.ExprContext ctx);
 	/**
 	 * Exit a parse tree produced by {@link CspParser#expr}.
 	 * @param ctx the parse tree
 	 */
-	void exitExpr(@NotNull CspParser.ExprContext ctx);
+	void exitExpr(CspParser.ExprContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link CspParser#number}.
+	 * @param ctx the parse tree
+	 */
+	void enterNumber(CspParser.NumberContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CspParser#number}.
+	 * @param ctx the parse tree
+	 */
+	void exitNumber(CspParser.NumberContext ctx);
+
 	/**
 	 * Enter a parse tree produced by {@link CspParser#comment}.
 	 * @param ctx the parse tree
 	 */
-	void enterComment(@NotNull CspParser.CommentContext ctx);
+	void enterComment(CspParser.CommentContext ctx);
 	/**
 	 * Exit a parse tree produced by {@link CspParser#comment}.
 	 * @param ctx the parse tree
 	 */
-	void exitComment(@NotNull CspParser.CommentContext ctx);
+	void exitComment(CspParser.CommentContext ctx);
 }
--- br.ufcg.edu.csp/src/br/ufcg/edu/csp/parser/CspVisitor.java
+++ br.ufcg.edu.csp/src/br/ufcg/edu/csp/parser/CspVisitor.java
@@ -1,6 +1,6 @@
-// Generated from Csp.g4 by ANTLR 4.4
 package br.ufcg.edu.csp.parser;
-import org.antlr.v4.runtime.misc.NotNull;
+
+// Generated from Csp.g4 by ANTLR 4.7
 import org.antlr.v4.runtime.tree.ParseTreeVisitor;
 
 /**
@@ -12,111 +12,149 @@ import org.antlr.v4.runtime.tree.ParseTreeVisitor;
  */
 public interface CspVisitor<T> extends ParseTreeVisitor<T> {
 	/**
-	 * Visit a parse tree produced by {@link CspParser#channelDecl}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitChannelDecl(@NotNull CspParser.ChannelDeclContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link CspParser#proc}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitProc(@NotNull CspParser.ProcContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link CspParser#set}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitSet(@NotNull CspParser.SetContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link CspParser#definitionLeft}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitDefinitionLeft(@NotNull CspParser.DefinitionLeftContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link CspParser#boolExp}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitBoolExp(@NotNull CspParser.BoolExpContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link CspParser#simple}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitSimple(@NotNull CspParser.SimpleContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link CspParser#simpleDefinition}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitSimpleDefinition(@NotNull CspParser.SimpleDefinitionContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link CspParser#boolOrAmb}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitBoolOrAmb(@NotNull CspParser.BoolOrAmbContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link CspParser#channelColonType}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitChannelColonType(@NotNull CspParser.ChannelColonTypeContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link CspParser#type}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitType(@NotNull CspParser.TypeContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link CspParser#any}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitAny(@NotNull CspParser.AnyContext ctx);
-	/**
 	 * Visit a parse tree produced by {@link CspParser#spec}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
-	T visitSpec(@NotNull CspParser.SpecContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link CspParser#number}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitNumber(@NotNull CspParser.NumberContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link CspParser#channelNames}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitChannelNames(@NotNull CspParser.ChannelNamesContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link CspParser#defnCallLeft}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitDefnCallLeft(@NotNull CspParser.DefnCallLeftContext ctx);
+	T visitSpec(CspParser.SpecContext ctx);
+
 	/**
 	 * Visit a parse tree produced by {@link CspParser#definition}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
-	T visitDefinition(@NotNull CspParser.DefinitionContext ctx);
+	T visitDefinition(CspParser.DefinitionContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#channelDecl}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitChannelDecl(CspParser.ChannelDeclContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#channelNames}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitChannelNames(CspParser.ChannelNamesContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#channelColonType}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitChannelColonType(CspParser.ChannelColonTypeContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#simpleDefinition}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSimpleDefinition(CspParser.SimpleDefinitionContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#assertDefinition}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAssertDefinition(CspParser.AssertDefinitionContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#definitionLeft}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDefinitionLeft(CspParser.DefinitionLeftContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#defnCallLeft}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDefnCallLeft(CspParser.DefnCallLeftContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#any}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAny(CspParser.AnyContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#checkConditionBody}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCheckConditionBody(CspParser.CheckConditionBodyContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#modelCheckType}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitModelCheckType(CspParser.ModelCheckTypeContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#type}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitType(CspParser.TypeContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#simple}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSimple(CspParser.SimpleContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#set}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSet(CspParser.SetContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#proc}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitProc(CspParser.ProcContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#boolExp}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitBoolExp(CspParser.BoolExpContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#boolOrAmb}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitBoolOrAmb(CspParser.BoolOrAmbContext ctx);
+
 	/**
 	 * Visit a parse tree produced by {@link CspParser#expr}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
-	T visitExpr(@NotNull CspParser.ExprContext ctx);
+	T visitExpr(CspParser.ExprContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link CspParser#number}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitNumber(CspParser.NumberContext ctx);
+
 	/**
 	 * Visit a parse tree produced by {@link CspParser#comment}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
-	T visitComment(@NotNull CspParser.CommentContext ctx);
+	T visitComment(CspParser.CommentContext ctx);
 }
